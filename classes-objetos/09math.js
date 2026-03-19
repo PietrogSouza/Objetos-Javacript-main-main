ab809b211286804b8af93469346fbaf2ccc39507
Pietro <pietro.g.souza@aluno.senai.br>
2026-03-19 08:39:29 -0300

    Criando uma função para gerar números aleatórios

---
+++ classes-objetos/09math.js
@@ -0,0 +1,27 @@
+const gerarNumeroAletorio = () => {
+    // Math.random() gera um número entre 0 e 1 (exclusivo)
+    let numeroAleatorio = Math.random();
+    console.log(`Sorteio: ${numeroAleatorio}`);
+
+    // Multiplicamos por 15 para obter um número entre 0 e 14 (exclusivo)
+    // numeroAleatorio *= 15; --> numeroAleatorio = numeroAleatorio * 15
+    numeroAleatorio *= 15;
+    console.log(`Multiplicar: ${numeroAleatorio}`)
+
+    // Math.floor() para arredondar para baixo para um número inteiro
+    numeroAleatorio = Math.floor(numeroAleatorio);
+    console.log(`Arredondar: ${numeroAleatorio}`);
+
+    // Somamos 1 para obter um número entre 1 e 15 (inclusivo)
+    numeroAleatorio += 1;
+    console.log(`Somar 1: ${numeroAleatorio}`);
+
+    // Retorna o número aleatório
+    return numeroAleatorio;
+}
+
+// Atribuindo a Função a uma variável
+let numeroSorteado = gerarNumeroAletorio();
+
+// Exibindo o número sorteado
+console.log(`Número sorteado: ${numeroSorteado}`)
