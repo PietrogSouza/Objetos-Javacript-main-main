38e2fc946ed16d1d26dba784744acf7f64b33506
Pietro <pietro.g.souza@aluno.senai.br>
2026-03-20 08:31:43 -0300

    Arrumando a estrutura HTML para fazer a lógica do JS, capturando os campos do inputs com o tipo range (slider), mostrando os valores dos slider na interface com uma função atualizarValorSlider

--- sorteio-numeros-aleatorios/numeros.js
+++ sorteio-numeros-aleatorios/numeros.js
@@ -1,2 +1,32 @@
+// Variáveis globais
+const quantidadeNumerosRecentes = 5;
 
+const sliderMin = document.querySelector('.intervalo__slider--min')
+const sliderMax = document.querySelector('.intervalo__slider--max')
+// console.log(sliderMin, sliderMax)
+
+
+
+
+const atualizarValorSlider = () =>{
+    const min = Number(sliderMin.value);
+    const max = Number(sliderMax.value);
+    console.log(min,max)
+
+    // Exibir o valor do slider na interface
+    document.querySelector('.intervalo__valor--min').textContent = min;
+    document.querySelector('.intervalo__valor--max').textContent = max;
+};
+    // Evento para atualizar o valor em tempo real
+    sliderMin.addEventListener('input', atualizarValorSlider);
+    sliderMax.addEventListener('input', atualizarValorSlider)
+
+// Iniciarlizar interface com valores atuais
+atualizarValorSlider();
+
+const gerarNumeroAleatorio = (min, max) =>{
+    let numeroAleatorio = Math.floor(Math.random() * (max - min + 1));
+    numeroAleatorio += min;
+    return numeroAleatorio;
+}
 
